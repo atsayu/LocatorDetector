600a380670c8ec2329185de1edef4bb2f4cde01f
Cong <125168053+buiftheescoong@users.noreply.github.com>
2024-07-29 13:56:41 +0700

    add word2vec for detecting

--- src/main/java/detect/word2vec/Word2Vec.java
+++ src/main/java/detect/word2vec/Word2Vec.java
@@ -1,2 +1,63 @@
-package detect.word2vec;public class Word2Vec {
+package detect.word2vec;
+
+//import org.json.simple.JSONArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+//import org.json.simple.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class Word2Vec {
+
+
+    public static void invokePythonProcess(String jsonString, String size, String pythonFilePath) throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonFilePath, jsonString, size);
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader readers = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        String lines = null;
+        while ((lines=reader.readLine()) != null) {
+            System.out.println("lines " + lines);
+        }
+
+        while ((lines= readers.readLine()) != null) {
+            System.out.println("Error lines" + lines);
+        }
+
+
+//        List<String> results = readProcessOutput(process.getInputStream());
+    }
+
+    public static String convertDataToJsonFormat(List<String> describedLocator, List<List<String>> describedElements) {
+        JSONObject object = new JSONObject();
+        JSONArray jsonArray = new JSONArray(describedLocator);
+        object.put(String.valueOf(-1), jsonArray);
+        int len = describedElements.size();
+        for (int i = 0; i < len; i++) {
+            object.put(String.valueOf(i), new JSONArray(describedElements.get(i)));
+        }
+        String objectToSTring = object.toString();
+        return objectToSTring;
+    }
+
+
+    public static void main(String[] args) {
+        List<String> describedLocator = Arrays.asList("sign", "in");
+        List<String> login = Arrays.asList("login", "button", "login", "login", "submit");
+        List<String> username = Arrays.asList("username", "username", "user", "name");
+        List<String> password = Arrays.asList("password", "password");
+        List<List<String>> describedElements = Arrays.asList(username, password, login);
+        String jsonString = convertDataToJsonFormat(describedLocator, describedElements);
+        System.out.println(jsonString);
+        try {
+            invokePythonProcess(jsonString, String.valueOf(3), "E:\\LAB UI\\UITestingLocatorDetector\\src\\main\\java\\detect\\word2vec\\findBestElement.py");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/detect/word2vec/findBestElement.py
+++ src/main/java/detect/word2vec/findBestElement.py
@@ -3,19 +3,22 @@ from gensim.models import Word2Vec
 from gensim.models.keyedvectors import KeyedVectors
 from sklearn.metrics.pairwise import cosine_similarity
 import sys
+import json5
 
 
-def findBestMatchElement(model, x, y):
+def findBestMatchElement(model, jsonObject, size):
     max_similarity = 0
-    for i in y:
-        similarity = model.n_similarity(x, y)
-        if (similarity > max_similarity)
+    index = -2
+    describedLocator = jsonObject[str(-1)]
+    print(describedLocator)
+    for i in range(size):
+        describedElement = jsonObject(str(i))
+        similarity = model.n_similarity(describedLocator, describedElement)
+        print(similarity)
+        if similarity > max_similarity:
             max_similarity = similarity
-    
-    
-
-    
-    
+            index = i
+    return index    
 # print(model.n_similarity(['sushi', 'shop'], ['japanese', 'restaurant']))
 # print(model.n_similarity(['save', 'and', 'end'], ['save', 'close', 'joomla', 'submitbutton','user','save']))
 # print(model.n_similarity(['login'], ['sign in']))
@@ -24,9 +27,13 @@ def findBestMatchElement(model, x, y):
 if __name__ == "__main__":
     path = r'E:\LAB UI\UITestingLocatorDetector\src\main\resources\GoogleNews-vectors-negative300.bin'
     model = KeyedVectors.load_word2vec_format(path, binary=True)
-    x = sys.argv[1]
-    y = sys.argv[2]
-    result = findBestMatchElement(model, x, y)
+    model.save("word2vec.model")
+    jsonString = sys.argv[1]
+    size = sys.argv[2]
+    print(jsonString)
+    print(size)
+    jsonObject = json5.loads(jsonString)
+    result = findBestMatchElement(model, jsonObject, size)
     print(result)
   
 
